4fb28522eeac3253eb7eb1318c7435221efb4a91
Hemna <waboring@hemna.com>
2021-10-08 12:53:44 -0400

    Added version for repeat service

--- aprsd_repeat_plugins/aprsd_repeat_plugins.py
+++ aprsd_repeat_plugins/aprsd_repeat_plugins.py
@@ -1,6 +1,8 @@
 import logging
 
-from aprsd import messaging, plugin, trace
+from aprsd import plugin, trace
+
+import aprsd_repeat_plugins
 
 
 LOG = logging.getLogger("APRSD")
@@ -10,9 +12,9 @@ class VersionPlugin(plugin.APRSDRegexCommandPluginBase):
 
     version = "1.0"
     # Look for any command that starts with w or W
-    command_regex = "^[wW]"
+    command_regex = "^[vV]"
     # the command is for ?
-    command_name = "weather"
+    command_name = "version"
 
     enabled = False
 
@@ -20,35 +22,10 @@ class VersionPlugin(plugin.APRSDRegexCommandPluginBase):
         # Do some checks here?
         self.enabled = True
 
-    def create_threads(self):
-        """This allows you to create and return a custom APRSDThread object.
-
-        Create a child of the aprsd.threads.APRSDThread object and return it
-        It will automatically get started.
-
-        You can see an example of one here:
-        https://github.com/craigerl/aprsd/blob/master/aprsd/threads.py#L141
-        """
-        if self.enabled:
-            # You can create a background APRSDThread object here
-            # Just return it for example:
-            # https://github.com/hemna/aprsd-weewx-plugin/blob/master/aprsd_weewx_plugin/aprsd_weewx_plugin.py#L42-L50
-            #
-            return []
-
     @trace.trace
     def process(self, packet):
-
         """This is called when a received packet matches self.command_regex."""
 
-        LOG.info("VersionPlugin Plugin")
+        LOG.info("VersionPlugin")
 
-        packet.get("from")
-        packet.get("message_text", None)
-
-        if self.enabled:
-            # Now we can process
-            return "some reply message"
-        else:
-            LOG.warning("VersionPlugin is disabled.")
-            return messaging.NULL
+        return aprsd_repeat_plugins.__version__
