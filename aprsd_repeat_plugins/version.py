ae5b833dddaab6a2cab68d505bffd4e9892644f8
Walter Boring <waboring@hemna.com>
2026-03-19 23:08:17 -0400

    feat: add tiered help to VersionPlugin

--- aprsd_repeat_plugins/version.py
+++ aprsd_repeat_plugins/version.py
@@ -3,15 +3,16 @@ import logging
 from aprsd import plugin
 
 import aprsd_repeat_plugins
+from aprsd_repeat_plugins.help import TieredHelpMixin
 
 LOG = logging.getLogger('APRSD')
 
 
-class VersionPlugin(plugin.APRSDRegexCommandPluginBase):
+class VersionPlugin(TieredHelpMixin, plugin.APRSDRegexCommandPluginBase):
     version = aprsd_repeat_plugins.__version__
-    # Look for any command that starts with w or W
+    # Look for any command that starts with v or V
     command_regex = '^[vV]'
-    # the command is for ?
+    # the command is for version
     command_name = 'version'
 
     enabled = False
@@ -19,6 +20,12 @@ class VersionPlugin(plugin.APRSDRegexCommandPluginBase):
     def setup(self):
         # Do some checks here?
         self.enabled = True
+
+    def help_basic(self) -> list[str]:
+        return ['v - shows REPEAT plugin version']
+
+    def help_full(self) -> list[str]:
+        return ['v or version - shows REPEAT plugin version']
 
     def process(self, packet):
         """This is called when a received packet matches self.command_regex."""
